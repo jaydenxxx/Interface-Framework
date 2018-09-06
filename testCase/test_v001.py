8ece6849c8d3c4618d5ad0c9652d7e136cdc93fc
jaydenxxx <yancyxxx@163.com>
2018-09-06 11:37:31 +0800

    新增用例

--- testCase/test_v001.py
+++ testCase/test_v001.py
@@ -3,7 +3,7 @@ from slwy_rest_framework.testModel import ModelCase
 import json
 
 
-class est_v001(ModelCase):
+class Test_v001_apiname(ModelCase):
     """
     v001版本机票查询接口
     """
@@ -11,6 +11,30 @@ class est_v001(ModelCase):
 
     # 用例1
     def test_v001(self):
+        """
+        航班查询中是否有CA航司
+        :return:
+        """
+        result_dict = json.loads(self.response.text)
+        airCodeList = []
+
+        for items in result_dict['data']['flights']:
+            airCodeList.append(items['airCode'])
+
+        assert "CA" in set(airCodeList)
+        print(airCodeList)
+
+    def test_v003(self):
+        result_dict = json.loads(self.response.text)
+        airCodeList = []
+
+        for items in result_dict['data']['flights']:
+            airCodeList.append(items['airCode'])
+
+        assert "CA" in set(airCodeList)
+        print(airCodeList)
+
+    def test_v004(self):
         result_dict = json.loads(self.response.text)
         airCodeList = []
 
@@ -37,8 +61,7 @@ class est_v001(ModelCase):
         assert "CA" in set(airCodeList)
         print(airCodeList)
 
-
-if __name__ == '__main__':
-    # est_v001().test_v001()
-    est_v001().test_v002()
-    pass
+# if __name__ == '__main__':
+#     # est_v001().test_v001()
+#     est_v001().test_v002()
+#     pass
